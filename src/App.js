d111ebd28967aa4a6aefaabb3a7d658909a3cdce
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2022-06-09 09:54:14 -0300

    Basic Router usage implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className=" bg-gray-50 h-screen flex">
-        <Login />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Layout />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,27 @@
+const Layout = (props) => {
+  return (
+    <div>
+      <nav>
+        <button>
+          <img src="" alt="" />
+        </button>
+        <div>
+          <button>
+            <img src="" alt="" />
+          </button>
+          <button>
+            <img src="" alt="" />
+          </button>
+          <button>
+            <img src="" alt="" />
+          </button>
+        </div>
+        <button>
+          <img src="" alt="" />
+        </button>
+      </nav>
+    </div>
+  );
+};
+
+export default Layout;
