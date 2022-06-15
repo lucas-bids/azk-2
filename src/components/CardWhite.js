f807bb832bb9a8bce7f036ce20ede2b18d7a65eb
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2022-06-15 11:22:35 -0300

    Update CardWhite.js

--- src/components/CardWhite.js
+++ src/components/CardWhite.js
@@ -1,7 +1,11 @@
 const CardWhite = (props) => {
-    return (
-        <div className="bg-white rounded-2xl shadow p-3 mt-4">{props.children}</div>
-    )
-}
+  return (
+    <Fragment>
+      <div className="bg-white rounded-2xl shadow p-3 mt-4">
+        {props.children}
+      </div>
+    </Fragment>
+  );
+};
 
 export default CardWhite;
