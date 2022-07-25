8ae07c588b32036edcd489a968fcb519bae9f959
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2022-07-25 14:22:10 -0300

    Create AltHeader.js

---
+++ src/components/header/AltHeader.js
@@ -0,0 +1,13 @@
+
+import DarkMode from "./DarkMode";
+
+const AltHeader = (props) => {
+  return (
+    <header className="pt-3 flex items-end justify-between">
+      <h1 className="text-2xl">{props.type}</h1>
+      <DarkMode />
+    </header>
+  );
+};
+
+export default AltHeader;
