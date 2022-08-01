8865edb6af96fe07c9c6c3556185b5dbbfdf7d73
lucas-bids <69700288+lucas-bids@users.noreply.github.com>
2022-08-01 10:42:44 -0300

    Beginning new custom http hook

--- src/components/UI/MenuBar.js
+++ src/components/UI/MenuBar.js
@@ -11,23 +11,35 @@ import reportsIconGray from "../../assets/images/icons/reports-gray.svg";
 import { NavLink } from "react-router-dom";
 
 const MenuBar = () => {
+
   return (
     <div className="w-1/12 flex justify-center">
       <div className="p-3 h-[800px] w-fit">
         <nav className=" bg-white rounded-2xl shadow h-full flex flex-col justify-between items-center">
-          <button>
-            <img className="w-20 p-4" src={purpleLogo} alt="" />
-          </button>
+          <NavLink to="/tasks">
+            <button>
+              <img className="w-20 p-4" src={purpleLogo} alt="" />
+            </button>
+          </NavLink>
           <div className="flex flex-col">
-              <img className="w-18 p-4" src={homeIcon} alt="" />
-            <button>
-            </button>
-            <button>
-              <img className="w-18 p-4" src={clientsIcon} alt="" />
-            </button>
-            <button>
-              <img className="w-18 p-4" src={reportsIcon} alt="" />
-            </button>
+            <NavLink to="/tasks">
+              {({isActive}) => (
+                <img className="w-18 p-4" src={isActive ? homeIcon : homeIconGray} alt="" />
+              )
+              }
+            </NavLink>
+            <NavLink to="/clients">
+            {({isActive}) => (
+                <img className="w-18 p-4" src={isActive ? clientsIcon : clientsIconGray} alt="" />
+              )
+              }
+            </NavLink>
+              <NavLink to="/reports">
+              {({isActive}) => (
+                <img className="w-18 p-4" src={isActive ? reportsIcon : reportsIconGray} alt="" />
+              )
+              }
+              </NavLink>
           </div>
           <button>
             <img className="w-18 p-4" src={logoutIcon} alt="" />
